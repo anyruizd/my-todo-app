dd150b44c6ea96e000694372703efe4b8d4d487f
anyruizd <anyruizd@gmail.com>
2019-04-02 11:26:36 -0500

    Add mock data

--- src/data/data.js
+++ src/data/data.js
@@ -2,4 +2,14 @@ import ItemsData from './ItemsData'
 
 const data = new ItemsData();
 
+data.add({
+  value: 'holiii',
+  completed: true
+})
+
+data.add({
+  value: 'Hello again',
+  completed: false
+})
+
 export default data;
