69be82544236f61feeabdce3f67bf61bffa496ae
anyruizd <anyruizd@gmail.com>
2019-04-01 19:51:12 -0500

    Polish components code

--- src/components/createItemsContainer.js
+++ src/components/createItemsContainer.js
@@ -1,13 +1,13 @@
 import createItem from './createItem'
 import data from './../data/data'
 
-export default function createItemsContainer() {
+export default function createItemsContainer(properties = {}) {
   const element = document.createElement('ul')
   element.classList.add('todos__items-container')
 
   data.subscribe('addItem', () => {
     const list = data.getList()
-    
+
     element.innerHTML = ''
     list.forEach(itemData => {
       const itemElement = createItem(itemData)
