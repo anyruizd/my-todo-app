47b141ade7c4b4d402e308ce969628c8ef0a074f
anyruizd <anyruizd@gmail.com>
2019-04-04 22:20:09 -0500

    Add item events handlers

--- src/components/Item/createItem.js
+++ src/components/Item/createItem.js
@@ -1,11 +1,19 @@
 export default function createItem(properties = {}) {
-  const { id, value, completed } = properties;
-
+  const {
+    id,
+    value,
+    completed,
+    onSelect,
+    onRemove,
+    onEdit,
+    onDoubleClick
+  } = properties;
+  
   const element = document.createElement('li')
   element.className = 'todos__item todos__item--showing'
 
   const template = `
-    <div class ="todos__item-checkbox">
+    <div class="todos__item-checkbox">
       <input ${completed ? "checked" : ''} type="checkbox" value="None" id="${id}" name="check"/>
       <label for="${id}"></label>
     </div>
@@ -15,6 +23,30 @@ export default function createItem(properties = {}) {
     `
   element.innerHTML = template
 
+  const checkboxElement = element.querySelector('input[type="checkbox"]')
+  const removeElement = element.querySelector('.todos__item-remove')
+  const itemTextElement = element.querySelector('.todos__item-text')
+  const itemEditElement = element.querySelector('.todos__item-edit')
+
+  checkboxElement.addEventListener('click', () => onSelect(id))
+  removeElement.addEventListener('click', () => onRemove(id))
+  itemTextElement.addEventListener('dblclick', () => onDoubleClick(id))
+  itemEditElement.addEventListener('keypress', () => onEdit(id))
+  
   return element
 }
 
+
+/**
+ * const itemElement = itemsListElement.querySelector('.todos__item:last-child')
+  const itemCheckbox = itemElement.querySelector('input[type="checkbox"]')
+  const itemText = itemElement.querySelector('.todos__item-text')
+  const itemEdit = itemElement.querySelector('.todos__item-edit')
+  const itemRemove = itemElement.querySelector('.todos__item-remove')
+
+  itemCheckbox.addEventListener('click', handleSelectItemEvent)
+  itemText.addEventListener('dblclick', handleEditTextEvent)
+  itemEdit.addEventListener('keypress', handleItemEditEvent)
+  itemRemove.addEventListener('click', handleRemoveItemEvent)
+ * 
+ */
--- src/components/ItemsContainer/createItemsContainer.js
+++ src/components/ItemsContainer/createItemsContainer.js
@@ -7,15 +7,39 @@ export default function createItemsContainer(properties = {}) {
 
   function renderItems() {
     const list = data.getList()
-
+    const events = {
+      onSelect,
+      onRemove,
+      onEdit,
+      onDoubleClick
+    }
     element.innerHTML = ''
     list.forEach(itemData => {
-      const itemElement = createItem(itemData)
+      const itemElement = createItem({...itemData, ...events})
+      console.log({...itemData, ...events})
       element.appendChild(itemElement)
     });
   }
+  
   renderItems();
   data.subscribe('addItem', renderItems)
+  data.subscribe('removeItem', renderItems)
+
+  function onSelect (id) {
+    console.log('onselect!', id)
+  }
+
+  function onRemove (id) {
+    data.remove(id)
+  }
+
+  function onEdit (id) {
+    console.log('onEdit!', id)
+  }
+
+  function onDoubleClick (id) {
+    console.log('onDoubleClick!', id)
+  }
 
   return element
 }
