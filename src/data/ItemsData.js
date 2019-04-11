515a98a478b71199df06e4265ab5e0eb965e5882
anyruizd <anyruizd@gmail.com>
2019-04-10 23:04:09 -0500

    Add item functionality

--- src/data/ItemsData.js
+++ src/data/ItemsData.js
@@ -13,12 +13,30 @@ export default class ItemsData extends PubSub {
       id: 'item-' + Math.round(Math.random() * 100000)
     }
     this.list = [...this.list, element];
-    this.publish('addItem', this.list);
+    this.publish('updateItem', this.list);
   }
 
   remove(id) {
     this.list = this.list.filter(element => element.id !== id);
-    this.publish('removeItem', this.list);
+    this.publish('updateItem', this.list);
+  }
+
+  select(id) {
+    this.list.forEach(element => {
+      if(element.id === id){
+        element.completed = !element.completed
+      }
+    });
+    this.publish('updateItem', this.list);
+  }
+
+  update(id, value) {
+    this.list.forEach(element => {
+      if(element.id === id){
+        element.value = value
+      }
+    })
+    this.publish('updateItem', this.list);
   }
 
   getList() {
