df1b7dc905b304b05b87b252ae289645ac3873c9
anyruizd <anyruizd@gmail.com>
2019-05-01 23:13:54 -0500

    Update pubusb test cases

--- src/data/PubSub.test.js
+++ src/data/PubSub.test.js
@@ -3,12 +3,11 @@
 import PubSub from './Pubsub'
 
 describe('PubSub()', () => {
-  const pubsub = new PubSub()
-  const onOpen = jest.fn()
-  const onClose = jest.fn()
-
   describe('subscribe()', () => {
     test('Should subscribe the new events and its callbacks', () => {
+      const pubsub = new PubSub()
+      const onOpen = jest.fn()
+      const onClose = jest.fn()
       const events = [
         { eventName: 'open', callback: onOpen },
         { eventName: 'close', callback: onClose }
@@ -24,25 +23,46 @@ describe('PubSub()', () => {
   })
 
   describe('unsubscribe()', () => {
-    test('Should unsubscribe the event and callback passed by', () => {
+    test('Should unsubscribe the event and callback passed', () => {
+      const pubsub = new PubSub()
+      const onClose = jest.fn()
+      pubsub.subscribe('close', onClose)
       pubsub.unsubscribe('close', onClose)
       const subscribers = pubsub.subscribers
-      expect(subscribers).toHaveLength(1)
-      expect(subscribers).toContainEqual({ eventName: 'open', callback: onOpen })
-      expect(subscribers).not.toContainEqual({ eventName: 'close', callback: onClose })
+      expect(subscribers).toHaveLength(0)
     })
   })
 
   describe('publish()', () => {
     test('Should not publish event when there are not subscriptions', () => {
+      const pubsub = new PubSub()
+      const onClose = jest.fn()
       pubsub.publish('close', 'Good bye!')
       expect(onClose).not.toHaveBeenCalled()
     })
 
-    test('Should publish event when published', () => {
+    test('Should call callback with arguments passed when published', () => {
+      const pubsub = new PubSub()
+      const onOpen = jest.fn()
+      pubsub.subscribe('open', onOpen)
       pubsub.publish('open', 'Hello!')
       expect(onOpen).toHaveBeenCalledTimes(1)
       expect(onOpen).toHaveBeenCalledWith('Hello!')
+    })
+
+    test('Should not call other callbacks different from the one related to event', () => {
+      const pubsub = new PubSub()
+      const onOpen = jest.fn()
+      const onClose = jest.fn()
+      const events = [
+        { eventName: 'open', callback: onOpen },
+        { eventName: 'close', callback: onClose }
+      ]
+      events.forEach(({ eventName, callback }) => {
+        pubsub.subscribe(eventName, callback)
+      })
+      pubsub.publish('open', 'Hello!')
+      expect(onClose).not.toHaveBeenCalled()
     })
   })
 })
