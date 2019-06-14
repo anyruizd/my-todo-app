79af9f221e289326f407fe7a035a86c2bb0376f8
anyruizd <anyruizd@gmail.com>
2019-06-13 23:54:01 -0500

    Add createItem testcases with no tests

---
+++ src/components/Item/createItem.test.js
@@ -0,0 +1,13 @@
+/* eslint-env jest */
+import createItem from './createItem'
+
+test.todo('Should return an li element')
+test.todo('Should render element with provided properties')
+test.todo('Should have item--selected class when completed true')
+test.todo('Should call onSelect when checkBocElement clicked')
+test.todo('Should call onSelect with id')
+test.todo('Should call onRemove when removeElement clicked')
+test.todo('Should call onRemove with id')
+test.todo('Shoould add item--editing class name when double click')
+test.todo('Should call onEdit when itemTextElement keyPressed')
+test.todo('Should call onEdit with id')
