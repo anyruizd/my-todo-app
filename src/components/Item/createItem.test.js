63386c79ba538a437769b5451bd82b671c16873f
anyruizd <anyruizd@gmail.com>
2019-06-14 23:43:11 -0500

    Add createItem first tests

--- src/components/Item/createItem.test.js
+++ src/components/Item/createItem.test.js
@@ -1,8 +1,38 @@
 /* eslint-env jest */
 import createItem from './createItem'
 
-test.todo('Should return an li element')
-test.todo('Should render element with provided properties')
+test('Should return an li element', () => {
+  const properties = {
+    id: 'item-12345',
+    value: 'Hola1',
+    completed: true,
+    onSelect: jest.fn(),
+    onRemove: jest.fn(),
+    onEdit: jest.fn()
+  }
+  const element = createItem(properties)
+  const expected = element.tagName
+  expect(expected).toBe('LI')
+})
+
+test('Should render element with provided properties', () => {
+  const properties = {
+    id: 'item-12345',
+    value: 'Hola1',
+    completed: true
+  }
+  const expected = `
+    <div class="item__checkbox">
+      <input checked="" type="checkbox" value="None" id="item-12345" name="check">
+      <label for="item-12345"></label>
+    </div>
+    <span class="item__text">Hola1</span>
+    <input class="item__edit" type="text" value="Hola1">
+    <button class="item__remove"></button>
+  `
+  const element = createItem(properties)
+  expect(element.innerHTML).toBe(expected)
+})
 test.todo('Should have item--selected class when completed true')
 test.todo('Should call onSelect when checkBocElement clicked')
 test.todo('Should call onSelect with id')
