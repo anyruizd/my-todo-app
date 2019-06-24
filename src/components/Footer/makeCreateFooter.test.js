9ee8336cda849d4d6ffa8a5d532ecc2889f25678
anyruizd <anyruizd@gmail.com>
2019-06-23 23:08:17 -0500

    Add filters test cases

---
+++ src/components/Footer/makeCreateFooter.test.js
@@ -0,0 +1,8 @@
+/* eslint-env jest */
+
+test.todo('Should return a function')
+test.todo('Should return a div element with footer class')
+test.todo('Should call createFilters')
+test.todo('Should call createLeftCounter')
+test.todo('Should call createClearButton')
+test.todo('Should return footer markup')
