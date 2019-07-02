821045aed171fa9092ad5487810f44fe2f168e48
anyruizd <anyruizd@gmail.com>
2019-07-01 23:26:17 -0500

    Add leftcounter tests

--- src/components/LeftCounter/makeCreateLeftCounter.test.js
+++ src/components/LeftCounter/makeCreateLeftCounter.test.js
@@ -1,8 +1,60 @@
 /* eslint-env jest */
+import makeCreateLeftCounter from './makeCreateLeftCounter'
 
-test.todo('Should return a function')
-test.todo('Should create a div element with left-counter class')
-test.todo('Should return the counter with initial state')
-test.todo('Should call data.subscribe')
-test.todo('Should subscribe to updateList with updateLeftCounter')
-test.todo('Should update left counter when updateList called')
+test('Should return a function', () => {
+  const createLeftCounter = makeCreateLeftCounter()
+  expect(createLeftCounter).toBeInstanceOf(Function)
+})
+
+test('Should create a div element with left-counter class', () => {
+  const dependencies = {
+    data: {
+      getActive: jest.fn().mockReturnValue([]),
+      subscribe: jest.fn()
+    }
+  }
+  const createLeftCounter = makeCreateLeftCounter(dependencies)
+  const leftCounterElement = createLeftCounter()
+
+  expect(leftCounterElement.tagName).toBe('DIV')
+  expect(leftCounterElement.getAttribute('class')).toBe('left-counter')
+})
+
+test('Should return the counter with initial state', () => {
+  const dependencies = {
+    data: {
+      getActive: jest.fn().mockReturnValue([]),
+      subscribe: jest.fn()
+    }
+  }
+  const createLeftCounter = makeCreateLeftCounter(dependencies)
+  const leftCounterElement = createLeftCounter()
+
+  expect(leftCounterElement.innerHTML).toBe('0 items left')
+})
+
+test('Should subscribe to updateList', () => {
+  const dependencies = {
+    data: {
+      getActive: jest.fn().mockReturnValue([]),
+      subscribe: jest.fn()
+    }
+  }
+  const createLeftCounter = makeCreateLeftCounter(dependencies)
+  createLeftCounter()
+
+  expect(dependencies.data.subscribe).toHaveBeenCalledWith('updateList', expect.anything())
+})
+
+test('Should update left counter from its initial state', () => {
+  const dependencies = {
+    data: {
+      getActive: jest.fn().mockReturnValue(['an item', 'two items']),
+      subscribe: jest.fn()
+    }
+  }
+  const createLeftCounter = makeCreateLeftCounter(dependencies)
+  const leftCounterElement = createLeftCounter()
+
+  expect(leftCounterElement.innerHTML).toBe('2 items left')
+})
